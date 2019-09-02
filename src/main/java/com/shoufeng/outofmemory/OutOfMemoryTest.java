cc46ddcf527353aa3edb526db2b9eb0173aaff0d
shoufeng <915855132@qq.com>
2019-09-02 15:54:43 +0800

    添加永久代溢出实例

--- src/main/java/com/shoufeng/outofmemory/OutOfMemoryTest.java
+++ src/main/java/com/shoufeng/outofmemory/OutOfMemoryTest.java
@@ -1,6 +1,7 @@
 package com.shoufeng.outofmemory;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 内存溢出
@@ -8,14 +9,34 @@ import java.util.ArrayList;
  * -Xms20M 初始堆大小
  * -Xmx20M 最大堆内存
  * -Xmn10M 年轻代大小
+ * -XX:PermSize10M 永久代大小
+ * -XX:MaxPermSize10M 最大永久代大小
  * @author shoufeng
  */
 public class OutOfMemoryTest {
-    public static void main(String[] args) {
+
+    //老生代内存溢出
+    public void method1(){
         ArrayList<byte[]> list = new ArrayList<byte[]>();
         while (true) {
             byte[] bytes = new byte[1024 * 1024];
             list.add(bytes);
         }
+    }
+
+    //常量池内存溢出（1.7之前） 元空间内存溢出
+    public void method2(){
+        List<String> list = new ArrayList<String>();
+        int i = 0 ;
+        while (true){
+            list.add(String.valueOf(i).intern());
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        OutOfMemoryTest outOfMemoryTest = new OutOfMemoryTest();
+        //outOfMemoryTest.method1();
+        outOfMemoryTest.method2();
     }
 }
